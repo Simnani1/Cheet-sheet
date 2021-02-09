15f7e369b871d14babd550d33b32a03e7698bb2b
simnani abbasi <simnaniabbasi441@gmail.com>
2021-02-09 16:02:27 +0530

    initial commit

---
+++ ForLoops.java
@@ -0,0 +1,11 @@
+class Git{
+	public static void main(String[] args) {
+		
+	
+	System.out.println("Welcome to git");
+
+System.out.println("hii");
+System.out.println("In dev branch");
+System.out.println("For Loop Changes");
+}
+}
---
+++ Hello_Worlds.java
@@ -0,0 +1,7 @@
+class Git{
+public static void main(String[] args) {
+	System.out.println("Welcome to git");
+	System.out.println("Hii");
+	System.out.println("New change in Dev Branch");
+}
+}
